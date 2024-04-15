58985214ded2762bac139f3c70ec68801adee62c
Miniii-git <125822323+Miniii-git@users.noreply.github.com>
2024-04-16 02:39:35 +0330

    Navigate from"/" to "/products"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, createContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Products from "./pages/Products";
 import Checkout from "./pages/Checkout";
 import ProductsDetails from "./pages/ProductsDetails";
@@ -20,6 +20,7 @@ function App() {
       <Header />
       <dataContext.Provider value={data}>
         <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductsDetails />} />
           <Route path="/checkout" element={<Checkout />} />
